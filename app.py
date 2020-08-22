e94ebddc13394904b4ea4681fa08766082400a0c
DukeNguyen_ <Triduc2311m@gmail.com>
2020-08-22 09:52:13 -0400

    Add dockerfile
    + Add dockerfile
    + Add app.py for testing azure deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+import secrets, os
+from flask import Flask
+app = Flask(__name__)
+os.environ['PORT'] = '5000'
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello Azure!</h1>"
+
+if __name__ == '__main__':
+    secret_key = secrets.token_hex(16)
+    app.config['SECRET_KEY'] = secret_key
+    port = int(os.environ['PORT'])
+    app.run(debug=True, host='0.0.0.0', port=port)
